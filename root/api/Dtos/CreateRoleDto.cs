6fa7787e3abf191dcc93b70dcc3fe9d46eb60bdc
paracetamol <140186933+apoo3301@users.noreply.github.com>
2024-11-30 23:19:53 +0100

    users register & login endpoints created

---
+++ root/api/Dtos/CreateRoleDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos
+{
+    public class CreateRoleDto
+    {
+        [Required(ErrorMessage = "Role name is required")]
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
---
+++ root/api/Dtos/RegisterDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        public List<string>? Roles { get; set; }
+    }
+}
